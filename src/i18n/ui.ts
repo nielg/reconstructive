1c2fb049d44b324775b6a9cbdbe057590cdb1235
Niel Geysen <niel.geysen@student.ucll.be>
2026-06-21 08:51:48 +0200

    add logo and favicon with some small changes

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,10 +9,10 @@ export const defaultLang = "nl";
 
 export const ui = {
   nl: {
-    "nav.aanpak": "onze aanpak",
-    "nav.aanbod": "aanbod",
-    "nav.impressies": "impressies",
-    "nav.about": "over ons",
+    "nav.aanpak": "ONZE AANPAK",
+    "nav.aanbod": "AANBOD",
+    "nav.impressies": "IMPRESSIES",
+    "nav.about": "OVER ONS",
 
     "footer.website": "De Website is gemaakt met Astro door:",
   },
