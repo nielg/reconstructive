4075666c99ba3219444cf1352df152da936e00c4
Niel Geysen <77360980+nielg@users.noreply.github.com>
2026-06-26 14:34:20 +0200

    Add Kalendar (#5)
    * Reduce font-size and space
    
    * apply feedback to home page
    
    * apply footer feedback
    
    * apply aanpak feedback
    
    * apply about feedback
    
    * Fix link to slug.astro
    
    * Add intro text and header styling
    
    * center section
    
    * fix multi lang post
    
    * Remove margin by text-margin
    
    * Add i18n for nl and en
    
    * Add animationQuotes
    
    * Remove extra text
    
    * Add animationQuotes
    
    * Add Kalendar
    
    ---------
    
    Co-authored-by: Niel Geysen <niel.geysen@student.ucll.be>

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type Kalender = {
+  datum: string;
+  title: string;
+  locatie: string;
+  prijs: string;
+  taal: string;
+  aantalDeelnemers: string;
+  voorWie: string;
+  overnachting: boolean;
+};
